30d76ef06c2e9265aaf298325dc2b86f052fde6d
italianshen <747976478@qq.com>
2021-09-20 18:38:40 +0800

    Initial Commit

---
+++ algorithm_oc_parctice_app/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  algorithm_oc_parctice_app
+//
+//  Created by Danny on 2021/9/20.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ algorithm_oc_parctice_app/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  algorithm_oc_parctice_app
+//
+//  Created by Danny on 2021/9/20.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
